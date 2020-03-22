2e6e863ca4e24094ab2c34fc4ad107b2e9c1083a
Vince <vince@modusinternet.com>
2020-03-21 21:41:01 -0700

    asdf

--- sw.js
+++ sw.js
@@ -1,5 +1,32 @@
-const cacheName='v2020.03.21-04';
+const cacheName='v2020.03.21-05';
 
+/* In order to get the listed resources to load properly once moved to Amazon's CloudFront servers you need to add this to your S3 bucket, under Permissions/CORS configuration:
+
+<?xml version="1.0" encoding="UTF-8"?>
+<CORSConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+<CORSRule>
+    <AllowedOrigin>https://PUTYOURDOMAINNAMEHERE.com</AllowedOrigin>
+    <AllowedMethod>GET</AllowedMethod>
+    <MaxAgeSeconds>3000</MaxAgeSeconds>
+    <AllowedHeader>Authorization</AllowedHeader>
+</CORSRule>
+</CORSConfiguration>
+
+
+Then you need to select the appropriate distribution under CloudFront and click the Behaviors tab.  Create or Edit an existing Behavior and select the following settings:
+
+Allowed HTTP Methods: GET, HEAD, OPTIONS
+Cache Based on Selected Request Headers: Whitelist
+Add these to the right box under Whitelist Headers:
+	Access-Control-Request-Headers
+	Access-Control-Request-Method
+	Origin
+
+
+Then click the 'Yes, Edit' button at the bottom and give it about 10 minutes to propagate through the system and test using Chrome.
+
+
+*/
 //https://d23cij6660kk94.cloudfront.net
 //https://s3-us-west-1.amazonaws.com/icondds.com-www-01
 
