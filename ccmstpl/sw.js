fa677739e76c9f5f0d7b621d4ba5c34e54daf8ea
Vince <vince@modusinternet.com>
2020-04-06 16:22:19 -0700

    asdf

--- ccmstpl/sw.js
+++ ccmstpl/sw.js
@@ -71,17 +71,17 @@ self.addEventListener('activate',e=>{
 addEventListener('fetch',e=>{
 	const {request}=e;
 	/* Always bypass for range requests, due to browser bugs. */
-	if(request.headers.has('range')) return;
+	if(e.request.headers.has('range')) return;
 	e.respondWith(async function(){
 		/* Try to get from the cache. */
-		const cachedResponse=await caches.match(request);
+		const cachedResponse=await caches.match(e.request);
 		if(cachedResponse) return cachedResponse;
 		/* Otherwise, get from the network. */
 		try{
-			return await fetch(request);
+			return await fetch(e.request);
 		}catch(err){
 			/* If this was a navigation, show the offline page. */
-			if(request.mode==='navigate'){
+			if(e.request.mode==='navigate'){
 				return caches.match('/{CCMS_LIB:_default.php;FUNC:ccms_lng}/offline.html');
 			}
 			/* Otherwise throw. */
