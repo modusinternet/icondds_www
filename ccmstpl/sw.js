31c970dae76d1d7523bfda9db1a6fc7507203817
Vince <vince@modusinternet.com>
2020-04-06 17:14:28 -0700

    asdf

--- ccmstpl/sw.js
+++ ccmstpl/sw.js
@@ -24,7 +24,7 @@ Add these to the right box under Whitelist Headers:
 Then click the 'Yes, Edit' button at the bottom and give it about 10 minutes to propagate through the system and test using Chrome.
 */
 
-const cacheName='{CCMS_LIB:_default.php;FUNC:ccms_lng}-2020.04.06-14';
+const cacheName='{CCMS_LIB:_default.php;FUNC:ccms_lng}-2020.04.06-15';
 
 var cacheFiles=[
 	/*
@@ -78,7 +78,7 @@ addEventListener('fetch',e=>{
 	const {request}=e;
 	/* Always bypass for range requests, due to browser bugs. */
 	if(request.headers.has('range')) return;
-	e.respondWith(async function(){
+	request.respondWith(async function(){
 		/* Try to get from the cache. */
 		const cachedResponse=await caches.match(request);
 		if(cachedResponse) return cachedResponse;
