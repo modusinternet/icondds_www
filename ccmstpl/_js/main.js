918beba0d326711e269b737a99cf566cc4bb267b
Vince <vince@modusinternet.com>
2020-04-09 12:04:43 -0700

    asdf

--- ccmstpl/_js/main.js
+++ ccmstpl/_js/main.js
@@ -362,23 +362,23 @@ window.addEventListener('beforeinstallprompt', (e) => {
 	deferredPrompt = e;
 	// Remove the 'hidden' class from the install button container
 	divInstall.classList.toggle('hidden', false);
-});
 
-butInstall.addEventListener('click', (e) => {
-	// hide our user interface that shows our A2HS button
-	//btnAdd.style.display = 'none';
-	divInstall.classList.toggle('hidden', true);
-	// Show the prompt
-	deferredPrompt.prompt();
-	// Wait for the user to respond to the prompt
-	deferredPrompt.userChoice.then((choiceResult) => {
-		if (choiceResult.outcome === 'accepted') {
-			console.log('User accepted the A2HS prompt');
-		} else {
-			console.log('User dismissed the A2HS prompt');
-		}
-		deferredPrompt = null;
-	});
+	butInstall.addEventListener('click', (e) => {
+		// hide our user interface that shows our A2HS button
+		//btnAdd.style.display = 'none';
+		divInstall.classList.toggle('hidden', true);
+		// Show the prompt
+		deferredPrompt.prompt();
+		// Wait for the user to respond to the prompt
+		deferredPrompt.userChoice.then((choiceResult) => {
+			if (choiceResult.outcome === 'accepted') {
+				console.log('User accepted the A2HS prompt');
+			} else {
+				console.log('User dismissed the A2HS prompt');
+			}
+			deferredPrompt = null;
+		});
+	});`
 });
 
 window.addEventListener('appinstalled', (event) => {
