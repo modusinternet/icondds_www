aa9783e3954b79af2e6ded6b76861770048eb396
Vince <vince@modusinternet.com>
2020-04-15 22:47:11 -0700

    asdf

--- ccmstpl/_js/sw.js
+++ ccmstpl/_js/sw.js
@@ -24,7 +24,7 @@ Add these to the right box under Whitelist Headers:
 Then click the 'Yes, Edit' button at the bottom and give it about 10 minutes to propagate through the system and test using Chrome.
 */
 
-const cacheName='{CCMS_LIB:_default.php;FUNC:ccms_lng}-2020.04.09-01';
+const cacheName='{CCMS_LIB:_default.php;FUNC:ccms_lng}-2020.04.15-01';
 
 var cacheFiles=[
 	/*
