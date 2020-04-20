8297588c3f07adb4ef44e7902845b81d92a0e67d
Vince <vince@modusinternet.com>
2020-04-19 17:42:02 -0700

    asdf

--- ccmstpl/sri.php
+++ ccmstpl/sri.php
@@ -3,7 +3,7 @@ header("Content-Type: text/html; charset=UTF-8");
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
-@include "ccmspre/config.php";
+@include $_SERVER["DOCUMENT_ROOT"] . "ccmspre/config.php";
 ?><!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -17,9 +17,6 @@ header("Pragma: no-cache");
 <?php if($_SERVER["REQUEST_URI"] == "/en/sri.html?flag=1") {
 	try {
 		$CFG["DBH"] = @new PDO("mysql:host=$CFG["DB_HOST"];dbname=$CFG["DB_NAME"]", $CFG["DB_USERNAME"], $CFG["DB_PASSWORD"], array(PDO::ATTR_PERSISTENT => true));
-		/* Great sites talking about how to handle the utf-8 character sets properly:
-		https://www.toptal.com/php/a-utf-8-primer-for-php-and-mysql
-		https://mathiasbynens.be/notes/mysql-utf8mb4 */
 		$CFG["DBH"]->exec("set names utf8mb4");
 		$CFG["DBH"]->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
