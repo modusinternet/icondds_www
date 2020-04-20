57fea9c5a056a77987b39cef5c92fea8a1bd824c
Vince <vince@modusinternet.com>
2020-04-19 20:32:55 -0700

    asdf

--- ccmstpl/sri.php
+++ ccmstpl/sri.php
@@ -38,11 +38,13 @@ if($_SERVER["REQUEST_URI"] == "/en/sri.html?flag=1") {
 				unset($result);
 				$data = file_get_contents($value);
 				$result = base64_encode(hash("sha384", $data, true));
+				echo "Retrieved " . $value . ".  sha384 string = " . $result . "<br>\n";
 
 
 
-				$qry = $CFG["DBH"]->prepare("INSERT INTO `sri` (`id`, `url`, `sri-code`) VALUES (NULL, :data, :result);");
-				$qry->execute(array(':data' => $data, ':result' => $result));
+				$qry = $CFG["DBH"]->prepare("INSERT INTO `sri` (`id`, `url`, `sri-code`) VALUES (NULL, :value, :result);");
+				$qry->execute(array(':value' => $value, ':result' => $result));
+				echo "Added to database.<br><br>\n";
 
 
 
