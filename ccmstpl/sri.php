89cbfe4b10f140c37233b9d696e7ae9afa48bd14
Vince <vince@modusinternet.com>
2020-04-20 14:42:50 -0700

    asdf

--- ccmstpl/sri.php
+++ ccmstpl/sri.php
@@ -22,7 +22,7 @@ $array = array(
 	"https://d23cij6660kk94.cloudfront.net/ccmstpl/_js/modernizr-3.6.0-custom-min.js",
 	"https://d23cij6660kk94.cloudfront.net/ccmstpl/_css/owl.carousel-2.3.4.min.css",
 	"https://d23cij6660kk94.cloudfront.net/ccmstpl/_js/owl.carousel.min.js",
-	"/{CCMS_LIB:_default.php;FUNC:ccms_lng}/_css/style-{CCMS_LIB:_default.php;FUNC:ccms_lng_dir}.css"
+	"/en/_css/style-ltr.css"
 );
 
 if($_SERVER["REQUEST_URI"] == "/en/sri.html?flag=1") {
