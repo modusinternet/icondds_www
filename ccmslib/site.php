c91b73c99fcfd91f20a9e8038a0f58e2e4d5fff8
Vince <vince@modusinternet.com>
2020-05-05 16:18:31 -0700

    asdf

--- ccmslib/site.php
+++ ccmslib/site.php
@@ -239,7 +239,7 @@ function csp_header() {
 
 		// Defines valid sources of stylesheets or CSS.
 		//"style-src 'self' 'unsafe-inline' *.cloudfront.net *.google.com *.googletagmanager.com *.google-analytics.com *.googleapis.com; ".
-		"style-src 'nonce-" . $CFG["nonce"] . "' www.googletagmanager.com; ";
+		"style-src 'self' 'nonce-" . $CFG["nonce"] . "' www.googletagmanager.com; ";
 
 		// Restricts the URLs which may be loaded as a Worker, SharedWorker or ServiceWorker.
 		//"worker-src 'self';";
