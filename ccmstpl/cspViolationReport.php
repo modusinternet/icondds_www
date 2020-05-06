9c5e8dfc90b1eb184e750693d54a45f23e2edb93
Vince <vince@modusinternet.com>
2020-05-06 15:51:49 -0700

    asdf

--- ccmstpl/cspViolationReport.php
+++ ccmstpl/cspViolationReport.php
@@ -23,7 +23,7 @@ $message="Following violations occured:<br><br>";
 // To send HTML mail, the Content-type header must be set
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-$headers .= 'From: Example Website <noreply@icondds.com>' . "\r\n";
+$headers .= 'From: icondds.com - CSP Violations <noreply@icondds.com>' . "\r\n";
 
 // Mail it
 mail($to, $subject, $message, $headers);
