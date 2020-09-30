5e10fee951719ae7a6f30661c81b69fe34b89435
modusinternet <vince@modusinternet.com>
2020-09-30 08:05:40 -0700

    asdf

--- ccmslib/site.php
+++ ccmslib/site.php
@@ -236,7 +236,7 @@ function csp_header() {
 		// 'unsafe-eval' is undesirable according to https://observatory.mozilla.org, but it's required by Google Custom Search Engine which doesn't properly support nonce yet. (May 7, 2020)
 		// 'unsafe-inline' is undesirable according to https://observatory.mozilla.org.
 		//"script-src 'nonce-" . $CFG["nonce"] . "' 'strict-dynamic' 'unsafe-eval' *.google.com *.googletagmanager.com; ".
-		"script-src 'nonce-" . $CFG["nonce"] . "' 'strict-dynamic' 'unsafe-eval';".
+		"script-src 'nonce-" . $CFG["nonce"] . "' 'strict-dynamic' 'unsafe-eval' https://www.google-analytics.com https://www.googletagmanager.com; ".
 
 		// Defines valid sources of stylesheets or CSS.
 		//"style-src 'self' 'unsafe-inline' *.cloudfront.net *.google.com *.googletagmanager.com *.google-analytics.com *.googleapis.com; ".
