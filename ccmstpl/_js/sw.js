35976a2940dcb994b5ea2ebb997c896ea9be321e
modusinternet <vince@modusinternet.com>
2020-10-06 13:28:16 -0700

    ASD

--- ccmstpl/_js/sw.js
+++ ccmstpl/_js/sw.js
@@ -231,3 +231,9 @@ self.addEventListener('fetch', (event) => {
       }
     })());
   }
+	// If our if() condition is false, then this fetch handler won't intercept the
+  // request. If there are any other fetch handlers registered, they will get a
+  // chance to call event.respondWith(). If no fetch handlers call
+  // event.respondWith(), the request will be handled by the browser as if there
+  // were no service worker involvement.
+});
