12b6643fd3bdf58511ff0e774370e1c51fefd184
modusinternet <vince@modusinternet.com>
2020-10-06 13:24:45 -0700

    asdf

--- ccmstpl/_js/sw.js
+++ ccmstpl/_js/sw.js
@@ -82,7 +82,7 @@ addEventListener('activate',e=>{
 
 /* Fetchs cached resources first, otherwise gets from the network.  If no
 network connection displays the offline page. */
-///*
+/*
 addEventListener('fetch',e=>{
 	const {request}=e;
 
@@ -104,7 +104,7 @@ addEventListener('fetch',e=>{
 		}
 	}());
 });
-//*/
+*/
 
 /*
 self.addEventListener('fetch', e => {
@@ -202,3 +202,32 @@ addEventListener('fetch', (event) => {
   }());
 });
 */
+
+
+self.addEventListener('fetch', (event) => {
+  // We only want to call event.respondWith() if this is a navigation request
+  // for an HTML page.
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        // First, try to use the navigation preload response if it's supported.
+        const preloadResponse = await event.preloadResponse;
+        if (preloadResponse) {
+          return preloadResponse;
+        }
+
+        const networkResponse = await fetch(event.request);
+        return networkResponse;
+      } catch (error) {
+        // catch is only triggered if an exception is thrown, which is likely
+        // due to a network error.
+        // If fetch() returns a valid HTTP response with a response code in
+        // the 4xx or 5xx range, the catch() will NOT be called.
+        console.log('Fetch failed; returning offline page instead.', error);
+
+        const cache = await caches.open(cacheName);
+        const cachedResponse = await cache.match('/{CCMS_LIB:_default.php;FUNC:ccms_lng}/offline.html');
+        return cachedResponse;
+      }
+    })());
+  }
