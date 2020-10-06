6815810c0825337a93dc63882a60e99f250a7adc
modusinternet <vince@modusinternet.com>
2020-10-06 00:49:56 -0700

    asdf

--- ccmstpl/_js/sw.js
+++ ccmstpl/_js/sw.js
@@ -158,12 +158,12 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(async function() {
 
     //const cache = await caches.open('cacheName');
-    const cachedResponse = await cache.match(event.request);
+    const cachedResponse = await caches.match(event.request);
     const networkResponsePromise = fetch(event.request);
 
     event.waitUntil(async function() {
       const networkResponse = await networkResponsePromise;
-      await cache.put(event.request, networkResponse.clone());
+      await caches.put(event.request, networkResponse.clone());
     }());
 
     // Returned the cached response if we have one, otherwise return the network response.
