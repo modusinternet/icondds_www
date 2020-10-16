f8140125d64b2a3e8269fbc97cc5bb354e859b2a
modusinternet <vince@modusinternet.com>
2020-10-16 13:38:32 -0700

    asdf

--- ccmstpl/contact-us-ajax.php
+++ ccmstpl/contact-us-ajax.php
@@ -8,9 +8,11 @@ include $_SERVER["DOCUMENT_ROOT"] . "/" . $CFG["LIBDIR"] . "/_default.php";
 
 $json = array();
 
+/*
 if(!strstr($_SERVER["HTTP_REFERER"], $CFG["DOMAIN"])) {
 	$json['error']['invalid_referer'] = "Invalid submission, your POST does not appeared to have been submitted from the " . $CFG["DOMAIN"] . " website. (HTTP_REFERER = ". $_SERVER["HTTP_REFERER"] . ")";
 }
+*/
 
 if(!ccms_badIPCheck($_SERVER["REMOTE_ADDR"])) {
 	$json['error']['invalid_badIPCheck'] = "There is a problem with your message, it can not be posted using this form from your current IP Address.  Please contact the website administrators directly by either phone or email if you feel this message is in error for more information.";
