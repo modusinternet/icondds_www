f9d9886917a53ebb423aaf8f8e0552df18f84a64
modusinternet <vince@modusinternet.com>
2022-04-02 03:22:47 -0700

    asdf

--- ccmstpl/_js/serviceworker.php
+++ ccmstpl/_js/serviceworker.php
@@ -43,7 +43,7 @@ var cacheFiles = [
 /*
 Analytics and Service Worker:
 https://developers.google.com/web/ilt/pwa/integrating-analytics#analytics_and_service_worker
-self.importScripts('/ccmstpl/examples/_js/analytics-helper.js');
+self.importScripts('/ccmstpl/_js/analytics-helper.js');
 
 Important resources used in the assembly of this services code:
 https://googlechrome.github.io/samples/service-worker/custom-offline-page/
@@ -102,7 +102,7 @@ self.addEventListener('fetch',(event) => {
 					/* The template being called was not found in cache and there is no internet connection at the moment so display the offline page instead.  The code below makes sure we're dispalying the appropriate offline template for the language that's currently selected by the client. */
 					const regex = /\/(([a-z]{2,3})(-[a-z0-9]{2,3})?)\//i;
 					const lng = event.request.url.match(regex);
-					const searchForThis = '/' + lng[1] + '/examples/offline.html';
+					const searchForThis = '/' + lng[1] + '/offline.html';
 					return caches.match(searchForThis);
 				})
 			})
