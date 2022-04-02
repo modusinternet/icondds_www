27c5d2dbf25f9dd40ae35ab269bba71fc60050a2
modusinternet <vince@modusinternet.com>
2022-04-02 03:53:16 -0700

    asdf

--- ccmstpl/manifest.php
+++ ccmstpl/manifest.php
@@ -1,26 +1,26 @@
-<?
-header("Content-Type: application/manifest+json; charset=UTF-8");
-header('Expires: ' . gmdate('D, d M Y H:i:s \G\M\T', time() + ($CFG["CACHE_EXPIRE"] * 60)));
-?>{
+<?php
+header("Content-Type: application/manifest+json; charset=utf-8");
+header("Expires: " . gmdate("D, d M Y H:i:s T", time() + ($CFG["CACHE_EXPIRE"] * 60)));
+?>{CCMS_DB_PRELOAD:all,index}{
 	"short_name": "ICONIC",
-	"name": "ICONIC Dentistry",
-	"description": "At ICONIC Dentistry we serve patients with the goal of achieving better oral and systemic health.",
+	"name": "{CCMS_DB:all,company-name}",
+	"description": "{CCMS_DB:index,description}",
 	"icons": [
 		{
 			"src": "{CCMS_LIB:site.php;FUNC:load_resource("AWS")}/ccmstpl/_img/ico/android-chrome-192x192.png",
 			"sizes": "192x192",
 			"type": "image/png",
-      "purpose": "any maskable"
+			"purpose": "maskable"
 		},{
 			"src": "{CCMS_LIB:site.php;FUNC:load_resource("AWS")}/ccmstpl/_img/ico/android-chrome-256x256.png",
 			"sizes": "256x256",
 			"type": "image/png",
-      "purpose": "any maskable"
+			"purpose": "maskable"
 		},{
 			"src": "{CCMS_LIB:site.php;FUNC:load_resource("AWS")}/ccmstpl/_img/ico/android-chrome-512x512.png",
 			"sizes": "512x512",
 			"type": "image/png",
-      "purpose": "any maskable"
+			"purpose": "maskable"
 		}
 	],
 	"start_url": "/",
