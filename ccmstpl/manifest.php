22473036bb39addfa18da9dd5d453a6a3cfcb5ab
modusinternet <vince@modusinternet.com>
2022-04-12 21:51:26 -0700

    asdf

--- ccmstpl/manifest.php
+++ ccmstpl/manifest.php
@@ -14,7 +14,7 @@ header("Expires: " . gmdate("D, d M Y H:i:s T", time() + ($CFG["CACHE_EXPIRE"] *
 			"src": "{CCMS_LIB:site.php;FUNC:load_resource("AWS")}/ccmstpl/_img/ico/android-chrome-192x192.png",
 			"sizes": "192x192",
 			"type": "image/png",
-			"purpose": "any"
+			"purpose": "maskable"
 		},{
 			"src": "{CCMS_LIB:site.php;FUNC:load_resource("AWS")}/ccmstpl/_img/ico/android-chrome-256x256.png",
 			"sizes": "256x256",
